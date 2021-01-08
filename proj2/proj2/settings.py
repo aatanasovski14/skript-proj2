2bfd1c47130042a36114f941207894631c076120
Aleksandar Atanasovski RM71/14 <aatanasovski14@raf.rs>
2021-01-08 01:40:32 +0100

    refix move key

--- proj2/proj2/settings.py
+++ proj2/proj2/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import my_sec_key
+import proj2.my_sec_key
+SECRET_KEY = proj2.my_sec_key.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
