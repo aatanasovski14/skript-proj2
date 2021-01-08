fc1a04b34957093a31d001d97d34866aab21a607
Aleksandar Atanasovski RM71/14 <aatanasovski14@raf.rs>
2021-01-08 02:16:48 +0100

    zapravo dodata baza

--- proj2/proj2/settings.py
+++ proj2/proj2/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'proj2_baza',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
